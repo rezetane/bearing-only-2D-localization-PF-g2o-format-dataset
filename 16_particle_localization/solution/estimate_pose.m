618aed200105c9807833edf8c55cb2c7f22b446e
rezetane <rezetane.sidali@gmail.com>
2018-01-06 01:48:01 +0000

    Update estimate_pose.m

--- 16_particle_localization/solution/estimate_pose.m
+++ 16_particle_localization/solution/estimate_pose.m
@@ -1,8 +1,5 @@
   function    best_estimate_id = estimate_pose(samples)
-    %devide weights
-   % d_w=min(weights);
-    %d_wmax_idx=zeros(1,length(weights));
-    %for i=1:length(weights)
+    
     N=length(samples);
      register_d=zeros(N);
      sum_es=sum(samples(1:2,:)');
@@ -14,14 +11,5 @@
        endfor
        
        [~,best_estimate_id]=min(register_d);
-     %sum of all samples x y 
-     %devide by N 
-     %for loop i = N
-         %compute distance estimate to each particle
-         %register in array n 
-         %end
-         %[~ ,estimate id ]=
-      
-    
-    
+
 endfunction
